7a60c9b12da3aba8607376283b6772c0b48c8708
Thomas Neider <azoskalias@gmail.com>
2024-04-19 18:40:06 -0400

    DetailedCleaningCompanyMVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DetailedCleaningCompanyMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
